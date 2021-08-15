fa5f119e02a47566af53f413321d660e401af3f6
abdullah <zyr-@hotmail.com>
2021-08-15 19:57:39 +0300

    messenger screen

---
+++ lib/MessengerScreen.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MessengerScreen extends StatelessWidget {
+  const MessengerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        titleSpacing: 20.0,
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 20.0,
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/23036167?s=400&u=1f8e3f99feb5ff6c574aa166766b8c62b275f300&v=4'),
+            ),
+            SizedBox(
+              width: 15.0,
+            ),
+            Text(
+              "Chats",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.camera_alt,
+                size: 16.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          IconButton(
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.edit,
+                size: 16.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    5.0,
+                  ),
+                  color: Colors.grey[300],
+                ),
+                padding: EdgeInsets.all(
+                  5.0,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      'search',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => buildStoryItem(),
+                  separatorBuilder: (context, index) => SizedBox(
+                    width: 20.0,
+                  ),
+                  itemCount: 10,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ListView.separated(
+                itemBuilder: (context, index) => buildChatItem(),
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 20.0,
+                ),
+                itemCount: 15,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildChatItem() => Row(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              CircleAvatar(
+                radius: 30.0,
+                backgroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/23036167?s=400&u=1f8e3f99feb5ff6c574aa166766b8c62b275f300&v=4'),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(
+                  bottom: 5.0,
+                  end: 3.0,
+                ),
+                child: CircleAvatar(
+                  radius: 7.0,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Abdullah Saleh Ahmed Alshamiri',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'hello my name is abdullah saleh ahmed I am from yemen ',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      child: Container(
+                        width: 7.0,
+                        height: 7.0,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '02:34 pm',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+
+  Widget buildStoryItem() => Container(
+        width: 60.0,
+        child: Column(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomEnd,
+              children: [
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/23036167?s=400&u=1f8e3f99feb5ff6c574aa166766b8c62b275f300&v=4'),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(
+                    bottom: 12.0,
+                    end: 12.0,
+                  ),
+                  child: CircleAvatar(
+                    radius: 7.0,
+                    backgroundColor: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              'Abdullah Mansour Ali Mansour',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      );
+}
